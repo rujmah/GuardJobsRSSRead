6868e6b1eaea53cf7c5e6323a791e7792b1a2845
rjjm <rjjm@rjjm.net>
2011-05-09 11:00:33 +0100

    added sample ads

--- main.rb
+++ main.rb
@@ -4,17 +4,13 @@ require 'rss/2.0'
 require 'open-uri'
 require 'haml'
 
-def get_feed (source)
+def get_rss (source)
   content = "" 
   open(source) {|s| content = s.read }
   rss = RSS::Parser.parse(content, false)
 end
-
-# feeds
-get ('/') {redirect '/feed'}
-get ('/:feed') do
-  feed = ""
-  case params[:feed]
+def get_feed (feed_name)
+  case feed_name
   when "MentalHealth"
     feed = "http://jobs.guardian.co.uk/jobsrss/?Industry=191&Salary_Min=30000&Salary_Max=39999&CountryCode=GB"
   when "Nursing"
@@ -23,10 +19,21 @@ get ('/:feed') do
     feed = "http://jobs.guardian.co.uk/jobsrss/?Industry=195&CountryCode=GB"
   end
 
-  @feed = get_feed(feed)
-  
-  haml :show
-
 end
 
-post ('/select') {redirect("/#{params[:feed]}")}
+# feeds
+get ('/') {redirect '/sample/MentalHealth'}
+get ('/style.css') { sass :style }
+get ('/jobs/:feed') do
+  feed = get_feed(params[:feed])
+  @feed = get_rss(feed)  
+  haml :show
+end
+post ('/jobs/select') {redirect("/jobs/#{params[:feed]}")}
+get ('/sample') {redirect("/sample/Nursing")}
+get ('/sample/:feed') do
+  @feed = get_rss(get_feed(params[:feed]))
+  haml :sample
+end
+post ('/sample/select') {redirect("/sample/#{params[:feed]}")}
+
